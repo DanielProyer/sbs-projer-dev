c667e8feb00096bd28c95e7a801a1aed553b1887
DanielProyer <DanielProyer@users.noreply.github.com>
2026-06-20 15:36:20 +0200

    feat(camt): Stichtag auf 11.03.2026 gesenkt

--- sbs_projer_app/test/camt_stichtag_test.dart
+++ sbs_projer_app/test/camt_stichtag_test.dart
@@ -2,9 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sbs_projer_app/services/camt/camt_stichtag.dart';
 
 void main() {
-  test('Stichtag ist 20.06.2026, davor nicht automatisierbar', () {
-    expect(CamtStichtag.stichtag, DateTime(2026, 6, 20));
-    expect(CamtStichtag.istAutomatisierbar(DateTime(2026, 6, 19)), isFalse);
-    expect(CamtStichtag.istAutomatisierbar(DateTime(2026, 6, 20)), isTrue);
+  test('Stichtag ist 11.03.2026, davor nicht automatisierbar', () {
+    expect(CamtStichtag.stichtag, DateTime(2026, 3, 11));
+    expect(CamtStichtag.istAutomatisierbar(DateTime(2026, 3, 10)), isFalse);
+    expect(CamtStichtag.istAutomatisierbar(DateTime(2026, 3, 11)), isTrue);
   });
 }
