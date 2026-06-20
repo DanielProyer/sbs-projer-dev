419d7d6482e6396f76a7d7ee31aeb47cb82a0977
DanielProyer <DanielProyer@users.noreply.github.com>
2026-06-20 11:47:24 +0200

    feat(camt): effektiver Zahlername aus AddtlNtryInf

---
+++ sbs_projer_app/test/zahlername_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sbs_projer_app/services/camt/zahlername.dart';
+
+void main() {
+  test('nutzt partyName wenn vorhanden', () {
+    expect(effektiverZahlername(partyName: 'Hotel X', additionalInfo: 'Gutschrift Y'), 'Hotel X');
+  });
+  test('extrahiert aus "Gutschrift <Name>"', () {
+    expect(effektiverZahlername(partyName: null, additionalInfo: 'Gutschrift Gastro Latina GmbH'),
+        'Gastro Latina GmbH');
+  });
+  test('null wenn nichts brauchbar', () {
+    expect(effektiverZahlername(partyName: null, additionalInfo: 'Saldovortrag'), isNull);
+  });
+}
